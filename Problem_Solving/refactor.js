3101a5faeb3b1f99c9e45dd985956ff9c433784d
Nurmukhammed <nureke92@gmail.com>
2020-05-20 18:14:22 +0600

    Update. Refactor charCount method to get rid of Regular Exp

--- Problem_Solving/refactor.js
+++ Problem_Solving/refactor.js
@@ -5,49 +5,71 @@
 
 // Refactor to check where char is a number/letter
 
-function charCount(str) {
-    const result = {};
-    for (let i = 0; i < str.length; i++) {
-        const char = str[i].toLowerCase();
-        if (/[a-z0-9]/.test(char)) {
-            if (result[char] > 0) {
-                result[char]++;
-            } else {
-                result[char] = 1;
-            }
-        }
-    }
-    return result;
-}
+// function charCount(str) {
+//     const result = {};
+//     for (let i = 0; i < str.length; i++) {
+//         const char = str[i].toLowerCase();
+//         if (/[a-z0-9]/.test(char)) {
+//             if (result[char] > 0) {
+//                 result[char]++;
+//             } else {
+//                 result[char] = 1;
+//             }
+//         }
+//     }
+//     return result;
+// }
 
 
-// Refactor with for...of
+// // Refactor with for...of
 
-function charCount(str) {
-    const result = {};
-    for (const char of str) {
-        char.toLowerCase();
-        if (/[a-z0-9]/.test(char)) {
-            if (result[char] > 0) {
-                result[char]++;
-            } else {
-                result[char] = 1;
-            }
-        }
-    }
-    return result;
-}
+// function charCount(str) {
+//     const result = {};
+//     for (const char of str) {
+//         char.toLowerCase();
+//         if (/[a-z0-9]/.test(char)) {
+//             if (result[char] > 0) {
+//                 result[char]++;
+//             } else {
+//                 result[char] = 1;
+//             }
+//         }
+//     }
+//     return result;
+// }
 
 // Refactor with one single line 
+// function charCount(str) {
+//     const result = {};
+//     for (const char of str) {
+//         char = char.toLowerCase();
+//         if (/[a-z0-9]/.test(char)) {
+//             result[char] = ++result[char] || 1;
+//         }
+//     }
+//     return result;
+// }
+
+// Refactor to get rid off Regular Exp
 function charCount(str) {
     const result = {};
     for (const char of str) {
         char = char.toLowerCase();
-        if (/[a-z0-9]/.test(char)) {
+        if (isAlphaNumberic(char)) {
             result[char] = ++result[char] || 1;
         }
     }
     return result;
 }
 
-// console.log(charCount('Hi there!'));
+function isAlphaNumberic(char) {
+  const code = char.charCodeAt(0);
+  if (!(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123)) { // lower alpha (a-z)
+      return false;
+    }
+    return true;
+}
+
+console.log(charCount('Hello EVEREBODY !!!'));
