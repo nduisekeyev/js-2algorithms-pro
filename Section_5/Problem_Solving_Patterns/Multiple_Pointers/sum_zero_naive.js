70837bc65ad4a4a2a8e380ea24a96e7846951eb4
Nurmukhammed <nureke92@gmail.com>
2020-05-31 16:41:44 +0600

    Add. Multiple Pointers pattern

---
+++ Section_5/Problem_Solving_Patterns/Multiple_Pointers/sum_zero_naive.js
@@ -0,0 +1,19 @@
+// Write a function called sumZero which accepts a sorted 
+// array of integers. The function should find the first pair 
+// where the sum is 0. Return an array that includes both 
+// values that sum to zero or undefined if a pair does not exist
+
+// sumZero([-3,-2,-1,0,1,2,3]) // [-3,3] 
+// sumZero([-2,0,1,3]) // undefined
+// sumZero([1,2,3]) // undefined
+
+function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+sumZero([-3,-2,-1,0,1,2,3])
--- Section_5/index.js
+++ Section_5/index.js
@@ -1,3 +1,5 @@
 import './Problem_Solving_Patterns/Frequency_Counter/same_naive.js'
 import './Problem_Solving_Patterns/Frequency_Counter/same_refactored.js'
 import './Problem_Solving_Patterns/Anagrams/anagram.js'
+import './Problem_Solving_Patterns/Multiple_Pointers/sum_zero_naive.js'
+import './Problem_Solving_Patterns/Multiple_Pointers/sum_zero_refactor.js'
