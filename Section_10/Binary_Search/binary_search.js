4d165348e51a2bb8e6b63446d79dbb28cfc5a196
Nurmukhammed <nureke92@gmail.com>
2020-08-30 22:29:20 +0600

    Fix. Binary search variables: start, end, middle (read only)

--- Section_10/Binary_Search/binary_search.js
+++ Section_10/Binary_Search/binary_search.js
@@ -1,23 +1,27 @@
+// Binary search exercise
+
+// This algorithm should be more efficient than linearSearch - you can read how to implement it here - https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search and here - https://www.topcoder.com/community/data-science/data-science-tutorials/binary-search/
+
 const states =['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
 
 function binarySearch(arr, value) {
-  const left = 0;
-  const right = arr.length - 1;
+  let start = 0;
+  let end = arr.length - 1;
 
-  var middle = Math.floor((start + end) / 2);
-    while(arr[middle] !== elem && start <= end) {
-        if (elem < arr[middle]) {
+  let middle = Math.floor((start + end) / 2);
+    while(arr[middle] !== value && start <= end) {
+        if (value < arr[middle]) {
             end = middle - 1;
         } else {
             start = middle + 1;
         }
         middle = Math.floor((start + end) / 2);
     }
-    if (arr[middle] === elem) {
+    if (arr[middle] === value) {
         return middle;
     }
     
     return -1;
 }
 
-binarySearch(states, 'Pennsylvania');
+console.log(binarySearch(states, 'Washington'));
